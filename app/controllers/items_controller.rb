eb03a220c3d09f6f48e877b620dfd584be99945c
Maksim <maksim@wsb-118-72b.local>
2021-05-17 17:52:21 +0300

    RestFULL and render/redirect

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,7 +3,9 @@ class ItemsController < ApplicationController
   skip_before_action :verify_authenticity_token
   def index
     @items = Item.all
+=begin
     render body: @items.map {|i|"#{i.name}:#{i.price}"}
+=end
   end
 
   def create
@@ -13,6 +15,38 @@ class ItemsController < ApplicationController
     else
       render json: item.errors, status: :unprocessable_entity
     end
+  end
+
+  def new;end
+  def show
+    unless (@item=Item.where(id:params[:id]).first)
+      render body: 'ggg'
+      end
+  end
+
+  def edit
+    unless (@item=Item.where(id:params[:id]).first)
+      render body: 'ggg'
+      end
+  end
+
+  def update
+    item = Item.where(id:params[:id]).first
+    if item.update(items_params)
+    redirect_to item_path
+    else
+      render body:'Poka'
+    end
+  end
+
+  def destroy
+    item = Item.where(id:params[:id]).first.destroy
+    if item.destroyed?
+      redirect_to items_path #redirect_to '/items'
+    else
+      render json: item.errors, status: :unprocessable_entity
+    end
+
   end
 
   private
